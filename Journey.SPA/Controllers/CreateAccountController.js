93221cfe84fd6540048b372a184879babc719156
Arnel Gajek <gaar1719@student.ju.se>
2018-04-16 13:53:05 +0200

    Added: All the Controllers for the SPA.

---
+++ Journey.SPA/Controllers/CreateAccountController.js
@@ -0,0 +1,20 @@
+﻿app.controller('CreateAccountController', function ($scope, $http, $route, $location) {
+    $scope.titleCreateAccount = "Skapa konto";
+    //$scope.createAccountData = {};
+
+    var accountUrl = "http://localhost:53201/api/Account/Register";
+
+    // Create new account:
+    $scope.createAccount = function () {
+        $http({
+            method: 'POST',
+            url: accountUrl,
+            data: $.param($scope.createAccountData),  // Passes in the data as a string.
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }  // Set the headers so Angular passing info as form data (not request payload)
+        }).then(function (data) { // Sends the user to the Login-page after successfully creating an account.
+            $location.url('/');
+        }, function (error) {
+            console.log(error);
+        });
+    };
+});
---
+++ Journey.SPA/Controllers/LoginController.js
@@ -0,0 +1,23 @@
+﻿app.controller('LoginController', function ($scope, $http, $route, $location, $window) {
+    $scope.titleLogin = "Journey";
+
+    var loginData = this;
+    loginData.token = "";
+
+    // Login to your account:
+    $scope.loginForm = function (loginData) {
+        var data = "grant_type=password&username=" + loginData.username + "&password=" + loginData.password;
+        $http.post('/token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).then(function (response) {
+
+        loginData.token = response.data.access_token;
+
+        }).then(function (data) {
+            $location.url('/Summary');
+        }, function (error) {
+            console.log(error);
+        });
+    };
+});
+
+// Glömt lösenord:
+// Ej obligatoriskt, gör detta om det finns tid över.
