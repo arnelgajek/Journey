8db0a061f6dc678a550c86d522ef340986025889
Arnel Gajek <gaar1719@student.ju.se>
2018-04-16 13:49:00 +0200

    Added: The ChatHub to the project.

---
+++ Journey.SPA/ChatSupport/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Journey
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public override Task OnConnected()
+        {
+            var connectionId = Context.ConnectionId;
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+    }
+}
