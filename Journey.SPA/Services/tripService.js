944ab46ee86117fe130ee3c831e4009fa267669a
Arnel Gajek <gaar1719@student.ju.se>
2018-04-16 14:05:04 +0200

    Added: Scipts and Services to the SPA.

---
+++ Journey.SPA/Services/tripService.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+app.factory('tripService', ['$http', 'ngAuthSettings', function ($http, ngAuthSettings) {
+
+    var serviceBase = ngAuthSettings.apiServiceBaseUri;
+
+    var tripsServiceFactory = {};
+
+    var _getTrips = function () {
+
+        return $http.get(serviceBase + 'api/trips').then(function (results) {
+            return results;
+        });
+    };
+
+    tripsServiceFactory.getTrips = _getTrips;
+
+    return tripsServiceFactory;
+
+}]);
