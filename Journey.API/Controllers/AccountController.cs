2ee2957ebec9b02c6ef465c5d1c108ddf1c68bea
Arnel Gajek <gaar1719@student.ju.se>
2018-04-17 10:38:41 +0200

    Edited: AccountController.

--- Journey.API/Controllers/AccountController.cs
+++ Journey.API/Controllers/AccountController.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿//using Journey.API.Models;
+//using Journey.API.Results;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 
 namespace Journey.API.Controllers
