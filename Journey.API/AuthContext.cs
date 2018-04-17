e8de2c822d7c547d3f5e574ddad5d8a0f8470bda
Arnel Gajek <gaar1719@student.ju.se>
2018-04-17 11:26:35 +0200

    Added&Edited: AuthContext.cs.

---
+++ Journey.API/AuthContext.cs
@@ -0,0 +1,22 @@
+﻿using Journey.API.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Journey.API
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
